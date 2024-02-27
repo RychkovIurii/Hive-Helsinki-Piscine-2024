16a78e47e4493e054b9b104c3fce1de7313a6a59
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-02-27 10:36:01 +0200

    C06 ex02 done

---
+++ C06/ex02/ft_rev_params.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 16:38:50 by irychkov          #+#    #+#             */
+/*   Updated: 2024/01/25 17:09:35 by irychkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i = i + 1;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = argc - 1;
+	while (i > 0)
+	{
+		ft_putstr(argv[i]);
+		i = i - 1;
+	}
+}
